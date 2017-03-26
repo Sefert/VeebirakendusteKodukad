2fe86a8c6aa07cec98fb3df7111ee890c77f2828
Marko Mõznikov <Marko Mznikov>
2017-03-26 18:29:48 +0300

    kuues kodukas

--- js/5s.js
+++ js/5s.js
@@ -1,17 +1,17 @@
-window.onload=function(){
-    if (window.location.href.match('5s.html') != null) {
+window.onload = function () {
+    if (window.location.href.match('5s.html') !== null) {
         var div = document.getElementById("neljas");
-        div.onclick=function(){
+        div.onclick = function(){
             console.log("tere"); 
             div.style.top = Math.random()*500+"px";
             div.style.left = Math.random()*500+"px";
             console.log(Math.random()*500); 
           };
     };
-    if (window.location.href.match('5sabakus.html') != null) {
+    if (window.location.href.match('5sabakus.html') !== null) {
         var div = document.getElementsByClassName("bead");
         window.addEventListener("mouseover", function(){
-        for (var i=0;i<div.length;i++){
+        for (var i = 0; i < div.length ; i++){
             if(window.getComputedStyle(div[i]).getPropertyValue("float") == "left"){
                 div[i].addEventListener("click", function(e){
                     console.log(i);
@@ -24,6 +24,31 @@ window.onload=function(){
                 });  
             };
         };
+        });
+    };
+    if (window.location.href.match('loop.html') !== null) {
+        $('li').first().addClass("active");
+        var li= document.getElementsByClassName("active");
+        $("#next").click(function() {
+            if($(li).is(":last-child")){
+                $(li).removeClass("active");
+                $('li').first().addClass("active");
+                li= document.getElementsByClassName("active");  
+            } else { 
+                $(li).next().addClass("active");
+                $(li).prev().removeClass("active");
+            };
+            
+        });
+        $("#prev").click(function() {
+            if($(li).is(":first-child")){
+                $(li).removeClass("active");
+                $('li').last().addClass("active");
+                li= document.getElementsByClassName("active");  
+            } else {
+                $(li).prev().addClass("active");
+                $(li).next().removeClass("active");
+            };
         });
     };
 };
